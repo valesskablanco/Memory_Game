00a3d04b701166a725188554701d81674cc02a2b
valesskablanco <valesskablanco@estudiantec.cr>
2022-03-20 20:26:25 -0600

    server

---
+++ server.h
@@ -0,0 +1,16 @@
+#ifndef server_h
+#define server_h
+
+#include <unistd.h>
+#include <stdio.h>
+#include <sys/socket.h>
+#include <stdlib.h>
+#include <netinet/in.h>
+#include <string.h>
+#include <iostream>
+#include <netdb.h>
+#include <arpa/inet.h>
+#include <string>
+#define PORT 8080
+#define server_ip "192.168.100.134"
+
